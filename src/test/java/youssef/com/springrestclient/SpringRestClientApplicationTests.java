210e45816f5b2c037d66ae6fd31a16a6925002ff
youssefhassan <youssefhassanshapan1995@gmail.com>
2024-08-30 22:19:16 +0300

    Initial Create

---
+++ src/test/java/youssef/com/springrestclient/SpringRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package youssef.com.springrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
